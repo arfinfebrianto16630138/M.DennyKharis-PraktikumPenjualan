f3841cb7ea2058a25958e32becda4d7136692464
Mdennykharis <46566011+Mdennykharis@users.noreply.github.com>
2021-03-07 02:54:30 -0800

    Add files via upload

---
+++ PraktikumPenjualan-main/src/main/java/com/sahrian/praktikumpenjualan/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sahrian.praktikumpenjualan;
+
+import db.Database;
+import java.sql.Connection;
+
+/**
+ *
+ * @author ahmad sahrian
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        
+        Database db = new Database();
+        Connection con = db.getConnection();
+    }
+}
---
+++ PraktikumPenjualan-main/src/main/java/model/JenisBarang.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ahmad sahrian
+ */
+public class JenisBarang {
+
+    public JenisBarang(Connection con) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setNamaJenisBarang(String namaJenisBarang) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean create() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setId(int idInt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Object getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getNamaJenisBarang() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ArrayList<Object> search(String keyword) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ArrayList<Object> read() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
